be7bc12adbc5fcd49b05463e860d4060d0c76d38
jessbrigs <71236279+jessbrigs@users.noreply.github.com>
2020-11-26 22:22:42 -0500

    Applied HTML/CSS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import "./App.css";
 import Weather from "./Weather.js";
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Weather />
+      <div className="container">
+        <Weather defaultCity="Toronto" />
+
+        <footer>
+          This project was coded by Jessica Brigantino and is{" "}
+          <a
+            href="https://github.com/jessbrigs/weather-react-2020"
+            target="blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,35 +1,33 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
     <div className="Weather">
-      <header>
-        <form id="location-search">
-          <div className="row">
-            <div className="col-7">
-              <input
-                className="form-location"
-                type="text"
-                placeholder="Enter a city..."
-                autocomplete="off"
-                id="the-city"
-              />
-            </div>
-            <div className="col-2">
-              <input className="submit-button" type="submit" value="Search" />
-            </div>
-            <div className="col-2">
-              <button className="current-location-btn" id="current-location">
-                Current Location
-              </button>
-            </div>
+      <form>
+        <div className="row">
+          <div className="col-2">
+            <input
+              className="form-location"
+              type="search"
+              placeholder="Enter a city..."
+              autocomplete="off"
+            />
           </div>
-        </form>
-      </header>
+          <div className="col-7">
+            <input className="submit-button" type="submit" value="Search" />
+          </div>
+          <div className="col-2">
+            <button className="current-location-btn" id="current-location">
+              Current Location
+            </button>
+          </div>
+        </div>
+      </form>
       <br />
       <section className="location">
-        <div className="current-date" id="current-date"></div>
-        <div className="city" id="city"></div>
+        <div className="city">Toronto</div>
+        <div className="current-date">Monday, November 30, 2020</div>
       </section>
       <section className="current-temperature">
         <div className="current-forecast-img">
@@ -39,28 +37,27 @@ export default function Weather() {
             id="weather-icon"
           />
         </div>
-        <span className="current-temp" id="current-temp">
-          {" "}
-        </span>
+        <ul>
+          <li className="weather-description">Windy</li>
+        </ul>
+        <span className="current-temp">16 </span>
         <span className="units">
-          <span id="celsius-link" className="active">
+          <span className="celsius-link" className="active">
             °C
           </span>{" "}
-          |<span id="fahrenheit-link">°F</span>
+          |<span className="fahrenheit-link">°F</span>
         </span>
-        <ul className="weather-description">
-          <li id="weather-description"></li>
-        </ul>
+
         <ul className="weather-details">
           <li>
-            Humidity: <span id="humidity"></span>%
+            Humidity: <span className="humidity"></span>70%
           </li>
           <li>
-            Wind: <span id="wind"></span> km/h
+            Wind: <span id="wind"></span>525 km/h
           </li>
         </ul>
         <h></h>
-      </section>{" "}
+      </section>
     </div>
   );
 }
